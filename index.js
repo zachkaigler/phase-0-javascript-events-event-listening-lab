f4eeee31d57285e0d4b95f0f188a5959c2d42a3b
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-03-09 19:52:47 -0500

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+    });
 }
+
+addingEventListener();
